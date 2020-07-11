6abbd0c6ef6cc6d5fb27e05f60184457b5f00979
Gabriel Bicudo Rosa <gabrielbicudorosa@gmail.com>
2020-07-10 22:53:12 -0300

    tirando key

--- testeapi.py
+++ testeapi.py
@@ -2,7 +2,8 @@ import json
 import requests
 import reu2
 
-key = "Bearer "
+
+key = "Bearer xxx"
 
 def acessarapi():  #apenas acesa a api e retorna o resultado
     headers = { 'authorization': key}
@@ -12,7 +13,7 @@ def acessarapi():  #apenas acesa a api e retorna o resultado
 
 def acessarapi2():#apenas acesa a api e retorna o resultado se houver mais de uma pagina para ser guardada
     headers = { 'authorization': key }
-    numpag = {'page_size': reu2.calculapagsize() ,'page_number':reu2.calculopag()}
+    numpag = {'page_size': reu2.calculapagsize(),'page_number':reu2.calculopag()}
     conn = requests.request("GET", "https://api.zoom.us/v2/accounts", headers=headers, params=numpag)
     print(len(conn.json()['accounts']))
     return(conn)
