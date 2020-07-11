e9ad3b984892b51ea4d723574b5781a57069e44b
Gabriel Bicudo Rosa <gabrielbicudorosa@gmail.com>
2020-07-10 22:27:43 -0300

    adicionado funcão calculapagesize

--- reu2.py
+++ reu2.py
@@ -5,22 +5,22 @@ import testeapi
 
 res = testeapi.acessarapi().json() #conn.jason()
 
-def calculopag():
+def calculopag():#verifica se o numero de paginas e maior que o numero da pagina atual, se sim ele da como parametro no acessarapi2 o numero da prox pag para coletar
     if res['page_count'] > res['page_number']:
        npzoom = res['page_number']
        while npzoom < res['page_count']:
            npzoom = npzoom + 1
            return(npzoom)
-    def calculapagsize():
-        z = res['page_size']
-        x = (res['total_records'] - z)
-        while x <= 0:
+    def calculapagsize():#calcula o pagsize necessário da ultima pagina
+        z = res['page_size'] #300
+        x = res['total_records'] #380
+        while x > 0:
            y = x - z
            x = y 
-           print(x)
-           return() 
+           if x > 0 and x < z:
+               continue
+           return(x) 
         
-           
 for paginasjson in [testeapi.acessarapi2().json()]:
     print([paginasjson], "/n")
 
--- testeapi.py
+++ testeapi.py
@@ -4,15 +4,15 @@ import reu2
 
 key = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ5Q2tydWs4bVNCdThYVW9yam1oSnpnIiwiZXhwIjoxNDk2MDkxOTY0MDAwfQ.XhXx65hbXEa8mBPcW7CNxsgxSGTKHhEws8bM3ncxMHo"
 
-def acessarapi():
+def acessarapi():  #apenas acesa a api e retorna o resultado
     headers = { 'authorization': key}
     numpag = {'page_number':1}
-    conn = requests.request("GET", "https://api.zoom.us/v2/accounts", headers=headers, params=numpag)
+    conn = requests.request("GET", "https://api.zoom.us/v2/accounts", headers=headers, params=numpag)  
     return(conn)
 
-def acessarapi2():
+def acessarapi2():#apenas acesa a api e retorna o resultado se houver mais de uma pagina para ser guardada
     headers = { 'authorization': key }
-    numpag = {'page_size': "" ,'page_number':reu2.calculopag()}
+    numpag = {'page_size': reu2.calculapagsize() ,'page_number':reu2.calculopag()}
     conn = requests.request("GET", "https://api.zoom.us/v2/accounts", headers=headers, params=numpag)
     print(len(conn.json()['accounts']))
     return(conn)
