4391cd6af4ce903e5053bcb681488601ec922b72
Gabriel Bicudo Rosa <gabrielbicudorosa@gmail.com>
2020-08-31 16:35:14 -0300

    coletando e arrumando dados de todas as contas enviadas pela api

--- reu2.py
+++ reu2.py
@@ -3,31 +3,16 @@ import json
 import testeapi
 
 
-res = testeapi.acessarapi().json() #conn.jason()
+def acessarapi(n_pag):  #apenas acesa a api e retorna o resultado
+    numpag = {'page_number':n_pag}
+    conn = requests.request("GET", "https://api.zoom.us/v2/accounts", headers=testeapi.headers, params=numpag).json()
+    contas = conn['accounts']
+    
+    for i in contas:
+        print[i, '\n']
 
-def calculopag():#verifica se o numero de paginas e maior que o numero da pagina atual, se sim ele da como parametro no acessarapi2 o numero da prox pag para coletar
-    if res['page_count'] > res['page_number']:
-       npzoom = res['page_number']
-       while npzoom < res['page_count']:
-           npzoom = npzoom + 1
-           return(npzoom)
-    def calculapagsize():#calcula o pagsize necessário da ultima pagina
-        z = res['page_size'] #300
-        x = res['total_records'] #380
-        while x > 0:
-           y = x - z
-           x = y 
-           if x > 0 and x < z:
-               continue
-           return(x) 
-        
-for paginasjson in [testeapi.acessarapi2().json()]:
-    print([paginasjson], "/n")
+    ###testeapi.lista_subcontas.append()
+    print(len(contas))  
+    return(conn)
 
-#print(res)
-#lista_contas = res['accounts'] 
 
-  
-#print(type(lista_contas))
-
-#print(len(lista_contas))
--- testeapi.py
+++ testeapi.py
@@ -1,19 +1,36 @@
 import json
 import requests
-import reu2
+
+key = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6InlDa3J1azhtU0J1OFhVb3JqbWhKemciLCJleHAiOjE1OTg5MDc1NjAsImlhdCI6MTU5ODkwMjE2MX0.iO0-2LF1_Dle8_sZ-2w4nghfUG_xz9Ky2nNzvYHBpuI"
+headers = { 'authorization': key}
+conn = requests.request("GET", "https://api.zoom.us/v2/accounts", headers=headers, params={'page_number':1}).json()
+
+n_pag = conn['page_number']
+
+paginas = conn['page_count']
+
+lista_paginas = []
+
+lista_subcontas = []
+
+def acessarapi(n_pag):  #apenas acesa a api e retorna o resultado
+    numpag = {'page_number':n_pag}
+    conn = requests.request("GET", "https://api.zoom.us/v2/accounts", headers=headers, params=numpag).json()
+    contas = conn['accounts']
+    
+    for i in contas:
+        print("\n")
+        for j in i:
+            print(j,"=",i[j])
 
 
-key = "Bearer xxx"
+    ###testeapi.lista_subcontas.append()
+    print(len(contas))  
+    return
 
-def acessarapi():  #apenas acesa a api e retorna o resultado
-    headers = { 'authorization': key}
-    numpag = {'page_number':1}
-    conn = requests.request("GET", "https://api.zoom.us/v2/accounts", headers=headers, params=numpag)  
-    return(conn)
+while n_pag <= paginas:
+    acessarapi(n_pag)
+    print("pagina numero:",n_pag)
+    n_pag = n_pag + 1
+    
 
-def acessarapi2():#apenas acesa a api e retorna o resultado se houver mais de uma pagina para ser guardada
-    headers = { 'authorization': key }
-    numpag = {'page_size': reu2.calculapagsize(),'page_number':reu2.calculopag()}
-    conn = requests.request("GET", "https://api.zoom.us/v2/accounts", headers=headers, params=numpag)
-    print(len(conn.json()['accounts']))
-    return(conn)
