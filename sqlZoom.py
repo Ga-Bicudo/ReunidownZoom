6a2e23d85b5211de54ad37954ff3a0719080d75c
Gabriel Bicudo Rosa <gabrielbicudorosa@gmail.com>
2020-09-04 12:37:56 -0300

    codigo inserindo todos os dados da api no banco

--- sqlZoom.py
+++ sqlZoom.py
@@ -3,7 +3,7 @@ import testeapi
 
 sql_user = {
     'user':'Gabriel',
-    'password':'Tg@briel10',
+    'password':'xxx',
     'host':'127.0.0.1',
     'port': '3306',
     'database':'testemysql'
@@ -17,8 +17,16 @@ cursor = cnx.cursor()
 
 
 for i in testeapi.lista_subcontas:
-    insert_clients = ("insert into clientes(id_cliente,nm_cliente,email_cliente,account_type) values ({0},{1},{2},{3})".format(i['id'],i['account_name'],i['owner_email'],i['account_type']))
-    cursor.execute(insert_clients)
+    insert_clients = ("insert into clientes(id_cliente,nm_cliente,email_cliente,account_type) values ('{0}','{1}','{2}','{3}')".format(i['id'],i['account_name'],i['owner_email'],i['account_type']))
+    #print(insert_clients)
+    cursor.execute('{}'.format(insert_clients))
 
+cnx.commit()
+
+print(cursor)
+
+cursor.close()
+
+cnx.close()
 
 
--- testeapi.py
+++ testeapi.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-key = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6InlDa3J1azhtU0J1OFhVb3JqbWhKemciLCJleHAiOjE1OTk3NjY5MjAsImlhdCI6MTU5OTE2MjEyMH0.P1_J61eyRXv_JQuUx0tl4cNlTZ_iEr-xiijeOgrbRsg"
+key = "Bearer xxx"
 headers = { 'authorization': key}
 conn = requests.request("GET", "https://api.zoom.us/v2/accounts", headers=headers, params={'page_number':1}).json()
 
@@ -11,8 +11,6 @@ n_pag = conn['page_number']
 
 paginas = conn['page_count']
 
-lista_paginas = []
-
 lista_subcontas = []
 
 def acessarapi(n_pag):  #apenas acesa a api e retorna o resultado
@@ -21,11 +19,7 @@ def acessarapi(n_pag):  #apenas acesa a api e retorna o resultado
     contas = conn['accounts']
     
     for i in contas: # i é cada dict que guarda a informação de cada conta.
-        """print("\n")
-        print(i)"""
         lista_subcontas.append(i) #adiciona todas as contas em uma somente lista.
-        """for j in i:
-            print(j,"=",i[j])"""
     return
 
 while n_pag <= paginas:
@@ -33,10 +27,7 @@ while n_pag <= paginas:
     ###print("pagina numero:",n_pag)
     n_pag = n_pag + 1
 
-    
-    
-print(lista_subcontas)
+   
+   
 print(len(lista_subcontas))
 
-#precisa apenas adicionar todos clientes em uma lista, 
-# cada cliente c seu indice e depois fazer um looping inserindo cada um em uma posição do banco
